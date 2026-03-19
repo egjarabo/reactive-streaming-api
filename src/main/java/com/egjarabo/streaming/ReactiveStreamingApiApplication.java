aba35ad9cfecfbd0c095924781cee6c76fe7d181
egjarabo <egjarabo@gmail.com>
2026-03-19 12:24:06 +0100

    chore: add docker-compose for local PostgreSQL development

---
+++ src/main/java/com/egjarabo/streaming/ReactiveStreamingApiApplication.java
@@ -0,0 +1,13 @@
+package com.egjarabo.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStreamingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveStreamingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egjarabo/streaming/ReactiveStreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egjarabo.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveStreamingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
